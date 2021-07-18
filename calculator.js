32fdad03f789ee0c018d81ec40779e388a3b9aba
James Spencer <jameskcs1254@gmail.com>
2021-07-18 11:57:53 +0100

    Add skeleton of operate function

--- calculator.js
+++ calculator.js
@@ -1,3 +1,16 @@
+//operate function
+
+const operate = function(array, func){
+
+}
+
+
+
+
+
+
+//CALCULATE FUNCTIONS BEYOND HERE
+
 const divide = function(a,b){
     return a/b;
 }
@@ -7,24 +20,24 @@ const divide = function(a,b){
 
 const add = function(a, b) {
 	return a + b;
-};
+}
 
 const subtract = function(a,b) {
 	return a-b;
-};
+}
 
 const sum = function(array) {
 	return array.reduce( (sum, e) => sum + e, 0);
-};
+}
 
 const multiply = function(array) {
   return array.reduce( (sum, e) => sum * e, 1);
-};
+}
 
 const power = function(a,b) {
   return a**b;
 	
-};
+}
 
 const factorial = function(n) {
 	if (n === 0) {
@@ -32,4 +45,4 @@ const factorial = function(n) {
   }else {
    return n * factorial(n-1);
   }
-};
+}
