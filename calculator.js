1c48b437895f62934e90e9463dd182b25c4deab7
James Spencer <jameskcs1254@gmail.com>
2021-07-23 13:47:10 +0100

    Change to module pattern, add operator capture function
    I wrapped the program in a function to get around the problem of global variables (module design pattern). Also started the function for capturing
    clicks on the operator buttons.

--- calculator.js
+++ calculator.js
@@ -1,66 +1,93 @@
-//populate function
-const populateDisplay = function(){
-  const display = document.querySelector('.display');
-  const numbers = document.querySelectorAll('.num');
 
-  numbers.forEach(number => number.addEventListener('click', () => {
-    display.innerHTML += number.innerHTML;
-  }));
+//run calculator function wrapping
 
-}
-
+const runCalculator = function(){
+  populateDisplay();
+  operatorCapture();
+  let firstOperand;
+  let secondOperand;
+  let operatorSelected;
 
 
+  //operator capture function
 
-//operate function
+  const operatorCapture = function() {
+    const operators = document.querySelectorAll('.operator');
+    const display = document.querySelector('.display');
 
-const operate = function(a, b, operator){
-  switch(operator) {
+    operators.forEach(operator => operator.addEventListener('click', () => {
+      firstOperand = display.innerHTML;
+      operatorSelected = operator.innerHTML;
+      secondOperand = ''
 
-    case '/':
-      return divide(a, b);
+      display.innerHTML = '';
 
-    case '+':
-      return add(a, b);
-    
-    case '-':
-      return subtract(a, b);
-    
-    case '*':
-      return multiply(a, b);
+    }));
+
   }
 
-}
+  //populate function
+  const populateDisplay = function(){
+    const display = document.querySelector('.display');
+    const numbers = document.querySelectorAll('.num');
 
+    numbers.forEach(number => number.addEventListener('click', () => {
+      display.innerHTML += number.innerHTML;
+    }));
 
-//CALCULATE FUNCTIONS
-
-const divide = function(a, b) {
-    return a / b;
-}
-
-const add = function(a, b) {
-	return a + b;
-}
-
-const subtract = function(a, b) {
-	return a - b;
-}
-
-
-const multiply = function(a, b) {
-  return a * b;
-}
-
-const power = function(a, b) {
-  return a ** b;
-
-}
-
-const factorial = function(n) {
-	if (n === 0) {
-    return 1;
-  }else {
-   return n * factorial(n-1);
   }
+
+  //operate function
+
+  const operate = function(a, b, operator){
+    switch(operator) {
+
+      case '/':
+        return divide(a, b);
+
+      case '+':
+        return add(a, b);
+      
+      case '-':
+        return subtract(a, b);
+      
+      case '*':
+        return multiply(a, b);
+    }
+
+  }
+
+
+  //CALCULATE FUNCTIONS
+
+  const divide = function(a, b) {
+      return a / b;
+  }
+
+  const add = function(a, b) {
+    return a + b;
+  }
+
+  const subtract = function(a, b) {
+    return a - b;
+  }
+
+
+  const multiply = function(a, b) {
+    return a * b;
+  }
+
+  const power = function(a, b) {
+    return a ** b;
+
+  }
+
+  const factorial = function(n) {
+    if (n === 0) {
+      return 1;
+    }else {
+    return n * factorial(n-1);
+    }
+  }
+
 }
